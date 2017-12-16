8c39bac0116745037aa834c8a128d4ca8b034ef0
luocheng2015 <444790866@qq.com>
2017-12-16 13:36:26 +0800

    HelloWorld提交

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @author 会飞的狼 on 2017/12/16.
  */
 @RestController
+@Api(value = "hello")
 public class HelloController {
 
     @RequestMapping("/hello")
+    @ApiOperation(value="Hello", notes="Hello示例")
     public String index() {
         return "Hello World";
     }
