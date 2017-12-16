29cd9bc7a4108c292858ffda06fc9999db05ee7f
luocheng2015 <444790866@qq.com>
2017-12-16 12:53:51 +0800

    HelloWorld提交

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,4 +1,4 @@
-package com.demo.controller;
+package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
